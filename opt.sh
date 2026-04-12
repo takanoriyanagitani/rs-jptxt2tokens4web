2309ff4bd863a4e770d7ac522339d258047e79e6
yanagitani <takanori.yanagitani@gmail.com>
2026-04-13 07:53:02 +0900

    chore: wasi related shells

---
+++ opt.sh
@@ -0,0 +1,9 @@
+iwasi=./rs_jptxt2tokens4web.wasm
+
+wasm-opt \
+	-Oz \
+	-o opt.wasm \
+	--enable-bulk-memory \
+	--enable-nontrapping-float-to-int \
+	--enable-simd \
+	"${iwasi}"
---
+++ wasi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export RUSTFLAGS='-C target_feature=+simd128'
+
+cargo \
+	build \
+	--target wasm32-wasip1 \
+	--profile release-wasi \
+	--features include-dummy-start
